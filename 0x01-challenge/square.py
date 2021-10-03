1f95d3551f8868ae874292fe8d57cc93a9f405fd
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-10-03 10:48:44 -0500

    fix error in function area of square more documentation and class in camel case and name of function perimeter and lowercase

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script that verify area and perimeter of square"""
+""" Script that verify area and perimeter of square """
 
 
-class Square:
-    """ New Class"""
+class Square():
+    """ New Class """
 
     def __init__(self, *args, **kwargs):
-        """ Values of constructor"""
+        """ Values of constructor """
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -17,15 +17,16 @@ class Square:
         return self.width * self.height
 
     def permiter_of_my_square(self):
+        """ Perimetrer of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
